7f30a04dae3c282a8bfe8615e56f4b0ad3f7f981
Saif Al-Dheferi <saif.raad.k@gmail.com>
2020-04-07 03:02:12 +0300

    Adding Tableview

--- TodoeyList/ViewController.swift
+++ TodoeyList/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController
+{
 
-    override func viewDidLoad() {
+    
+    let itemArray=["Buy Some Eggs","Go to School","Get Some Milk"]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "ToDItemCell", for: indexPath)
+        cell.textLabel?.text=itemArray[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if let cell = tableView.cellForRow(at: indexPath)
+        {
+            if cell.accessoryType == .checkmark
+            {
+               cell.accessoryType = .none
+            }
+            else
+            {
+               cell.accessoryType = .checkmark
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+     }
 
 }
-
